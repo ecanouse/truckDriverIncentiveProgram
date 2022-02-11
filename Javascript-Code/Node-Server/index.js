74b6f2865064d1ca5facc817274cf672262ef925
Devin <Devin.j.narula@gmail.com>
2022-02-10 19:53:32 -0500

    Creating test endpoint

--- Javascript-Code/Node-Server/index.js
+++ Javascript-Code/Node-Server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors')
+
 
 const PORT = process.env.PORT || 4000;
 
 const app = express()
+app.use(cors())
+
+app.get('/test', (req, res) => {
+    res.json({message: 'Node Test Successful!'})
+});
 
 app.listen(PORT, () => {
     console.log(`Node server listening on port ${PORT}`)
-})
+});
