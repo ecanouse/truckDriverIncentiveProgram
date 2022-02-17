56390b0c8a67e0550d19316af887a01e829c4567
cdwise <56054770+cdwise@users.noreply.github.com>
2022-02-16 21:15:05 -0500

    Progress on Login Page. Not yet complete

--- Javascript-Code/react-client/src/pages/Login.js
+++ Javascript-Code/react-client/src/pages/Login.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
+import './Login.css';
 
 class Login extends Component{
   render() {
     return (
-      <h1>
-          Login Page
-      </h1>
+      <div className='LoginPage'>
+        <header className='Login-Header'>
+          <img src="teamLogo.png" alt="The Mad Lads Team Logo" width="250" height="100"></img>
+        </header>
+        <body className='Signin-Box'>
+          <h2><br></br>Sign-In</h2>
+          <p>
+            <span class='Seperate-SI-UN'></span>
+            Username
+          </p>
+          <input type="text" id="username" name="username" placeholder='username' size="50"></input>
+
+          <p>Password</p>
+          <input type="text" id="username" name="username" placeholder='username' size="50"></input>
+
+          <p class='Reset-UN-PASS'>Having trouble signing in?</p> 
+        </body>
+      </div>
+      
+      
     );
   }
 }
