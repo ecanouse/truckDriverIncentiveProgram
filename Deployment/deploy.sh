a57ab7535dd81a1907a4e98d0f6a3426e9712d37
Devin Narula <dnarula@clemson.edu>
2022-02-23 00:50:29 +0000

    Deleted Deployment

--- Deployment/deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ssh -o StrictHostKeyChecking=no -i "labsuser.pem" ubuntu@ec2-54-87-232-198.compute-1.amazonaws.com 'cd S22-Team04-goodwin.binz.canouse.lasher.narula.wise; git pull; cd Javascript-Code/react-client; npm run build; cd ..; forever start Node-Server/index.js'
