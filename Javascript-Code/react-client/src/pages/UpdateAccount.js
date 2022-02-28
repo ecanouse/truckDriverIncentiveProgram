d5072b8eab0536346b08383f9b57901a61775c2d
cdwise <56054770+cdwise@users.noreply.github.com>
2022-02-28 18:28:44 -0500

    small edits

--- Javascript-Code/react-client/src/pages/UpdateAccount.js
+++ Javascript-Code/react-client/src/pages/UpdateAccount.js
@@ -1,11 +1,51 @@
 import React, {Component} from 'react';
+import './UpdateAccount.css'
 
 class UpdateAccount extends Component{
   render() {
     return (
-      <h1>
-          Update Account Info
-      </h1>
+      <body>
+        <div className='UpdateAccount'>
+          <header className='UpdateAcc-Header'>
+              <img src="teamLogo.png" alt="The Mad Lads Team Logo" width="250" height="100"></img>
+            <nav className='Nav'>
+              <button href='AdminHome' className='NavButtons'>Home</button>
+              <button href='PointHistory' className='NavButtons'>Points</button>
+              <button href='CatalogPurchase' className='NavButtons'>Catalog</button>
+              <button href='UpdateAccount' className='NavButtons'>Settings</button>
+            </nav>
+            <ul className='UpdateAccLout-UpAcc'>
+              <img src='DefaultProfPic.png' alt='Default Profile Picure' width='40' height='40'/>
+              <li><a href='UpdateAccount'>Username</a></li>
+              <li><a href='Home'>Logout</a></li>
+            </ul>
+          </header>
+          <div className='UpdateAcc-Body'> 
+            <label className='inputs' for='userFirstName'><br/>Update your First Name<br/></label>
+              <input type='text' id='userFirstName' name='userFirstName' placeholder='John' size='45'></input>
+            <label className='inputs' for='userLastName'><br/>Update your Last Name<br/></label>
+              <input type='text' id='userLastName' name='userLastName' placeholder='Doe' size='45'></input>
+            <label className='inputs' for='userBirthday'><br/>Update your Birthday<br/></label>
+              <input type='date' id='userBirthday' name='userBirthday'></input>
+            <label className='inputs' for='userName'><br/>Username<br/></label>
+              <input type='text' id='userName' name='userName' placeholder='jondoe' size='45'></input>  
+            <label className='inputs' for='userEmail'><br/>Update your Email<br/></label>
+                <input type='email' id='userEmail' name='userEmail' placeholder='johndoe@email.com' size='45'></input>
+            <label className='inputs' for='userPassword'><br/>Update your Password<br/></label>
+                <input type='password' id='userPassword' name='userPassword' placeholder='password' size='45'></input> 
+            <label className='inputs' for='userPhoneNum'><br/>Update for Phone Number<br/></label>
+              <input type='tel' id='userPhoneNum' name='userPhoneNum' pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" placeholder='888 888 8888' size='45'></input>
+            
+            <label className='inputs' for='userProfilePicture'><br/>Update your Profile Picture<br/></label>
+              <input type="file" id='userProfilePicture' name='userProfilePicture' accept="image/*"></input> 
+            <button className='saveChanges'>Save Changes</button>
+
+          </div>
+          <footer className='UpdateAcc-Footer'>
+            <img src="teamLogo.png" alt="The Mad Lads Team Logo" width="200" height="70"></img>
+          </footer>
+        </div>
+      </body>
     );
   }
 }
