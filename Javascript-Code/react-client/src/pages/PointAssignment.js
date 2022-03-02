3718d6f7bc4b12681eaba522936edc9e961d207d
Devin <Devin.j.narula@gmail.com>
2022-03-02 09:59:59 -0500

    Adding Point Assignment UI

--- Javascript-Code/react-client/src/pages/PointAssignment.js
+++ Javascript-Code/react-client/src/pages/PointAssignment.js
@@ -2,6 +2,46 @@ import React, {Component} from 'react';
 import './PointAssignment.css'
 
 class PointAssignment extends Component{
+  state = {
+    add: true,
+    value: 0,
+    driver: '',
+    comment: ''
+  };
+
+  handleChange = (event) => {
+    const target = event.target;
+    const name = target.name
+    const value = target.value
+    
+    this.setState({
+      [name]: value
+    });
+
+  };
+
+  selectAdd = () => {
+    this.setState({
+      add: true
+    });
+  };
+
+  selectSub = () => {
+    this.setState({
+      add: false
+    });
+  };
+
+  submit = (event) => {
+    event.preventDefault();
+    console.log("Submit!")
+    this.setState({
+      add: true,
+      value: 0,
+      driver: '',
+      comment: ''
+    });
+  }
   render() {
     return (
       <div className='PointAssignPage'>
@@ -20,9 +60,69 @@ class PointAssignment extends Component{
           </ul>
         </header>
         
-        <body>
-          
-        </body>
+        
+        <form onSubmit={this.submit} className='point-form'>
+          <div className='form-item'>
+            <label htmlFor="addsub">Add or Subtract Points?</label>
+              <div className='point-addbuttons' >
+                <div className='point-addsub' style={{backgroundColor: this.state.add ? 'rgb(0, 200, 0)' : 'rgb(223, 223, 223)'}} onClick={this.selectAdd}>
+                  <div>+</div>
+                </div>
+                <div className='point-addsub' style={{backgroundColor: !this.state.add ? 'rgb(200, 0, 0)' : 'rgb(223, 223, 223)'}} onClick={this.selectSub}>
+                  <div>-</div>
+                </div>
+              </div>
+          </div>
+
+          <div className='form-item'>
+            <label htmlFor="value">Point Value</label>
+            <input 
+              type="number" 
+              id="value" 
+              name="value"
+              placeholder={0} 
+              value={this.state.value}
+              onChange={this.handleChange}
+              min={1}
+            />
+          </div>
+
+          <div className='form-item'>
+          <label htmlFor="driver">Driver</label>
+            <input 
+              list='driver-list' 
+              name='driver' 
+              id='driver'
+              value={this.state.driver}
+              onChange={this.handleChange}
+              required
+            />
+            <datalist id="driver-list">
+                <option value="Driver 1"/>
+                <option value="Driver 2"/>
+                <option value="Driver 3"/>
+                <option value="Driver 4"/>
+                <option value="Driver 5"/>
+            </datalist>
+          </div>
+
+          <div className='form-item'>
+          <label htmlFor="driver">Comment</label>
+          <textarea
+            cols="40" 
+            rows = "7"
+            id="comment" 
+            name="comment" 
+            placeholder="Additional Comments"
+            value={this.state.comment}
+            onChange={this.handleChange}
+            required
+            minLength={1}
+            maxLength={100}
+          />
+          </div>
+          <input className="point-submit" type="submit" value="Submit"/>
+        </form>
         
         <footer className='PointAssign-Footer'>
           <img src="teamLogo.png" alt="The Mad Lads Team Logo" width="200" height="70"></img>
