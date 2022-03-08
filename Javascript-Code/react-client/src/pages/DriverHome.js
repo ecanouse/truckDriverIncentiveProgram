6563d91edc2cabf2ba871c41c9e2457e23569bf1
cdwise <56054770+cdwise@users.noreply.github.com>
2022-03-07 20:46:36 -0500

    UIs for Sprint 5
    Created basic UI for 3 UIs

--- Javascript-Code/react-client/src/pages/DriverHome.js
+++ Javascript-Code/react-client/src/pages/DriverHome.js
@@ -9,10 +9,18 @@ class DriverHome extends Component{
           <link rel='icon' href='websiteicon.png'/>
           <img src="SponsorLogo.png" alt="Sponsor Logo" width="250" height="100"></img>
           <nav className='Nav'>
-            <button href='DriverHome' className='NavButtons'>Home</button>
-            <button href='PointHistory' className='NavButtons'>Points</button>
-            <button href='CatalogPurchase' className='NavButtons'>Catalog</button>
-            <button href='UpdateAccount' className='NavButtons'>Settings</button>
+            <a href='DriverHome'>
+            <button href='DriverHome' className='NavButtons' >Home</button>
+            </a>
+            <a href='PointHistory'>
+              <button className='NavButtons'>Points</button>
+            </a>
+            <a href='CatalogPurchase'>
+              <button className='NavButtons'>Catalog</button>
+            </a>
+            <a href='UpdateAccount'>
+              <button className='NavButtons'>Settings</button>
+            </a>
           </nav>
           <ul className='DriverLout-UpAcc'>
             <img src='DefaultProfPic.png' alt='Default Profile Picure' width='40' height='40'/>
