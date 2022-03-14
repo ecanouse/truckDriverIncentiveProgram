3a32ca266a50a2f088c9e22f4f1b71a157d9cb1d
jbinz <jbinz@clemson.edu>
2022-03-14 19:34:19 -0400

    re-formatted index js

---
+++ Javascript-Code/Node-Server/endpoints/signup-attempt.js
@@ -0,0 +1,63 @@
+module.exports = function( app, connection ) {
+    app.post('/signup-attempt', (req, res) => {
+
+        console.log('Received signup attempt');
+        console.log(req.body);
+        // get input
+        let username = req.body.username;
+        let password = req.body.password;
+        let firstname = req.body.firstname;
+        let lastname = req.body.lastname;
+        let confirmpassword = req.body.confirmpassword;
+        let email = req.body.email;
+        let phone = req.body.phone;
+      
+        //make sure user entered each required field
+        if( username && password && firstname && lastname && confirmpassword && email && phone) {
+          if(confirmpassword == password){
+            //check to make sure that username and email are not repeats of other accounts
+            qstr = "SELECT COUNT (*) AS count FROM new_schema.USER WHERE username = '"+username+"' OR email = '"+email+"'";
+            connection.query(qstr, function(err, result, fields) {
+              if(err) console.log(err);
+              console.log(result);
+              
+              //const repeat = Object.keys(result).length === 0;
+              string = JSON.stringify(result);
+              json = JSON.parse(string);
+              count = json[0].count;
+      
+              if(count > 0){
+                res.send({success: false, msg: "Username or email belongs to another account"});
+                console.log('repeat user');
+                res.end()
+              }else{
+        
+                encryptPass = crypt.getHash(password);
+                //console.log('Got sign up information.');
+                console.log(encryptPass);
+                qstr = "INSERT INTO new_schema.USER (lname, fname, username, password, email, phone, usertype) VALUES ('"+lastname+"', '"+firstname+"', '"+username+"', '"+encryptPass+"', '"+email+"', '"+phone+"', 0)";
+                connection.query(qstr, function(err, result, fields) {
+                  if(err) console.log(err);
+                  console.log(result);
+                  //res.redirect('/DriverHome');
+                  res.send({success: true, msg: "Creating Account..."});
+                });
+              }
+              //sql query to confirm information and create an account
+              //if correct --> res.redirect('/home');
+              //else --> give error message
+            });
+            
+      
+          }else {
+            res.send({success: false, msg: "Passwords do not match"});
+            console.log('passwords do not match');
+            res.end()
+          }
+        }else{
+          res.send({success: false, msg: "Please fill out all fields"});
+          console.log('no sign up info.');
+          res.end()
+        }
+      });
+}
--- Javascript-Code/Node-Server/index.js
+++ Javascript-Code/Node-Server/index.js
@@ -50,6 +50,8 @@ connection.connect(function(err) {
 require('./endpoints/sponsorinfo')(app, connection);
 require('./endpoints/points')(app, connection);
 require('./endpoints/usertype')(app, connection);
+require('./endpoints/login-attempt')(app, connection)
+require('./endpoints/signup-attempt')(app, connection)
 
 app.get('/test', (req, res) => {
     connection.query('SELECT * FROM test.test_table', (err, results) => {
@@ -68,165 +70,19 @@ app.get('/test', (req, res) => {
 // I will move this into it's own file/work on the structure of the node server in sprint 5
 // - joey
 
-app.post('/login-attempt', (req, res) => {
-  console.log('Recieved login attempt');
-  console.log(req.body);
-  // get input
-  let username = req.body.username;
-  let password = req.body.password;
-  //make sure username & password exist
-  if( username && password && !password.includes("\'") && !password.includes("\"") ) {
-
-    //hash password
-    // res.send('got username & password');
-    // console.log('Got username and password.');
-    //sql query to compare password
-    //if correct --> res.redirect('/home');
-    //else --> give error message
-
-    var testing = false; //variable for testing fxns
-
-    //clean username input to prevent SQL injections
-    const clean_username = username.split(" ");
-
-    //create accounts for testing
-    if( testing ) {
-
-      //get hashed password
-      var hpass = crypt.getHash(password);
-
-      //create new driver with hashed password
-      // username: driver
-      // password: driverpassword
-      const driver_query = "insert into USER values (2, -1, 'Guy', 'Driver', 'driver', '" + hpass + "', 'driver@email.net', '0987654567', 0);";
-
-      //create new sponsor with hashed password
-      // username: sponsor
-      // password: sponsorpassword
-      const sponsor_query = "insert into USER values (3, -1, 'Girl', 'Sponsor', 'sponsor', '" + hpass + "', 'sponsor@email.net', '0987654569', 1);";
-
-      // create new amin with hashed password
-      // username: admin
-      // password: adminpassword
-      const admin_query = "insert into USER values (4, -1, 'Person', 'Admin', 'admin', '" + hpass + "', 'admin@email.net', '0987654561', 2);";
-      // connection.query(admin_query);
-    }
-
-    var sel_query;
-    //allow users to sign in via email
-    if( clean_username[0].includes("@") ) {
-      sel_query = "SELECT password, userType, uID from new_schema.USER where email = \"" + clean_username[0] + "\";";
-    }
-    else {
-      sel_query = "SELECT password, userType, uID from new_schema.USER where username = \"" + clean_username[0] + "\";";
-    }
+// ------------------ start login stuff
 
 
-    //poll db
-    connection.query(sel_query, function(err, result, fields) {
-      if(err) console.log(err);
-      console.log(result);
-      
-      const isEmpty = Object.keys(result).length === 0;
 
-      //case for successful login
-      if( (!isEmpty) && crypt.validatePassword(password, result[0].password) ) {
-        session=req.session;
-        session.userid=result[0].uID;
-        console.log("Password Match!");
-        logs.recordLogin(username, true, connection);
-        res.send({success: true, userType: result[0].userType});
-      }
-      //case for unsuccessful logins
-      else if (isEmpty) {
-        console.log("Username/Email Not Recognized");
-        logs.recordLogin(username, false, connection);
-        res.send({success: false, msg: "Username/Email Not Recognized"});
-      }
-      else if (!isEmpty) {
-        console.log("Username and Password did not match");
-        logs.recordLogin(username, false, connection);
-        res.send({success: false, msg: "Username and Password did not match"});
-      }
-    });
-  } 
-  else {
-    logs.recordLogin(username, false, connection);
-    res.send({success: false});
-    console.log('no username/password.');
-    res.end()
-  }
 
-});
+// ------------------------------ end login stuff 
 
-app.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.send({success: true});
-});
-
+// start signup stuff
 
 //elise working on signup
-app.post('/signup-attempt', (req, res) => {
 
-  console.log('Received signup attempt');
-  console.log(req.body);
-  // get input
-  let username = req.body.username;
-  let password = req.body.password;
-  let firstname = req.body.firstname;
-  let lastname = req.body.lastname;
-  let confirmpassword = req.body.confirmpassword;
-  let email = req.body.email;
-  let phone = req.body.phone;
 
-  //make sure user entered each required field
-  if( username && password && firstname && lastname && confirmpassword && email && phone) {
-    if(confirmpassword == password){
-      //check to make sure that username and email are not repeats of other accounts
-      qstr = "SELECT COUNT (*) AS count FROM new_schema.USER WHERE username = '"+username+"' OR email = '"+email+"'";
-      connection.query(qstr, function(err, result, fields) {
-        if(err) console.log(err);
-        console.log(result);
-        
-        //const repeat = Object.keys(result).length === 0;
-        string = JSON.stringify(result);
-        json = JSON.parse(string);
-        count = json[0].count;
-
-        if(count > 0){
-          res.send({success: false, msg: "Username or email belongs to another account"});
-          console.log('repeat user');
-          res.end()
-        }else{
-  
-          encryptPass = crypt.getHash(password);
-          //console.log('Got sign up information.');
-          console.log(encryptPass);
-          qstr = "INSERT INTO new_schema.USER (lname, fname, username, password, email, phone, usertype) VALUES ('"+lastname+"', '"+firstname+"', '"+username+"', '"+encryptPass+"', '"+email+"', '"+phone+"', 0)";
-          connection.query(qstr, function(err, result, fields) {
-            if(err) console.log(err);
-            console.log(result);
-            //res.redirect('/DriverHome');
-            res.send({success: true, msg: "Creating Account..."});
-          });
-        }
-        //sql query to confirm information and create an account
-        //if correct --> res.redirect('/home');
-        //else --> give error message
-      });
-      
-
-    }else {
-      res.send({success: false, msg: "Passwords do not match"});
-      console.log('passwords do not match');
-      res.end()
-    }
-  }else{
-    res.send({success: false, msg: "Please fill out all fields"});
-    console.log('no sign up info.');
-    res.end()
-  }
-});
+// end signup stuff
 
 app.use(express.static(path.join(__dirname, "../react-client", 'build')));
 
