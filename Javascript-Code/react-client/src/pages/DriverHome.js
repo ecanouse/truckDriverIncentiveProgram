f236eda3ad2868713b46dfd6c5a694017a7b899a
cdwise <56054770+cdwise@users.noreply.github.com>
2022-03-14 22:03:27 -0400

    Update for sprint 6
    Progress for this sprint

--- Javascript-Code/react-client/src/pages/DriverHome.js
+++ Javascript-Code/react-client/src/pages/DriverHome.js
@@ -3,32 +3,54 @@ import './DriverHome.css'
 
 class DriverHome extends Component{
   render() {
-    return (
-      <div className='DriverHomePage'>
-        <header className='Driver-Header'>
-          <img src="SponsorLogo.png" alt="Sponsor Logo" width="250" height="100"></img>
-          <nav className='Nav'>
-            <button href='DriverHome' className='NavButtons'>Home</button>
-            <button href='PointHistory' className='NavButtons'>Points</button>
-            <button href='CatalogPurchase' className='NavButtons'>Catalog</button>
             <button href='UpdateAccount' className='NavButtons'>Settings</button>
-          </nav>
           <ul className='DriverLout-UpAcc'>
-            <img src='DefaultProfPic.png' alt='Default Profile Picure' width='40' height='40'/>
-            <li><a href='UpdateAccount'>Username</a></li>
-            <li><a href='Home'>Logout</a></li>
-          </ul>
-        </header>
-        <body>
-          
-          <select className='SortByDrop' id='SortByDrop'>
-            <option disabled selected hidden>Sort By</option>
-            <option>All Time</option>
-            <option>Last 24 Hours</option>
-            <option>Last 7 Days</option>
-            <option>Last 30 Days</option>
-          </select>
-        </body>
+    if (this.state.isDriver){
+      return (
+        <div className='DriverHomePage'>
+          <header className='Driver-Header'>
+            <link rel='icon' href='websiteicon.png'/>
+            <img src="SponsorLogo.png" alt="Sponsor Logo" width="250" height="100"></img>
+            <nav className='Nav'>
+              <a href='DriverHome'>
+                <button href='DriverHome' className='NavButtons' >Home</button>
+              </a>
+              <a href='PointHistory'>
+                <button className='NavButtons'>Points</button>
+              </a>
+              <a href='CatalogPurchase'>
+                <button className='NavButtons'>Catalog</button>
+              </a>
+              <a href='UpdateAccount'>
+                <button className='NavButtons'>Settings</button>
+              </a>
+
+              <select>
+              <option>select</option>
+              <option href='DriverHome'>Home</option>
+              <option href='PointHistory'>Points</option>
+              <option href='CatalogPurchase'>Catalog</option>
+              <option href='UpdateAccount'>Settings</option>
+            </select>
+            </nav>
+  
+
+            <ul className='DriverLout-UpAcc'>
+              <img src='DefaultProfPic.png' alt='Default Profile Picure' width='40' height='40'/>
+              <li><a href='UpdateAccount'>Username</a></li>
+              <li><a href='Home'>Logout</a></li>
+            </ul>
+          </header>
+          <body>
+            
+            <select className='SortByDrop' id='SortByDrop'>
+              <option disabled selected hidden>Sort By</option>
+              <option>All Time</option>
+              <option>Last 24 Hours</option>
+              <option>Last 7 Days</option>
+              <option>Last 30 Days</option>
+            </select>
+          </body>
 
         <footer className='Driver-Footer'>
           <img src="teamLogo.png" alt="The Mad Lads Team Logo" width="200" height="70"></img>
@@ -36,6 +58,6 @@ class DriverHome extends Component{
       </div>
     );
   }
-}
+}}
 
 export default DriverHome;
--- Javascript-Code/react-client/src/pages/PointHistory.js
+++ Javascript-Code/react-client/src/pages/PointHistory.js
@@ -3,26 +3,59 @@ import './PointHistory.css'
 
 class PointHistory extends Component{
   render() {
-    return (
-      <div className='PointHistoryPage'>
-        <header className='PointHistory-Header'>
-          <img src="SponsorLogo.png" alt="Sponsor Logo" width="250" height="100"></img>
-          <nav className='Nav'>
-            <button href='DriverHome' className='NavButtons'>Home</button>
-            <button href='PointHistory' className='NavButtons'>Points</button>
-            <button href='CatalogPurchase' className='NavButtons'>Catalog</button>
-            <button href='UpdateAccount' className='NavButtons'>Settings</button>
-          </nav>
-          <ul className='PointHistoryLout-UpAcc'>
-            <img src='DefaultProfPic.png' alt='Default Profile Picure' width='40' height='40'/>
-            <li><a href='UpdateAccount'>Username</a></li>
-            <li><a href='Home'>Logout</a></li>
-          </ul>
-        </header>
-        
-        <body>
+    if (this.state.isDriver){
+      return (
+        <div className='PointHistoryPage'>
+          <header className='PointHistory-Header'>
+            <img src="SponsorLogo.png" alt="Sponsor Logo" width="250" height="100"></img>
+            <nav className='Nav'>
+              <a href='DriverHome'>
+                <button href='DriverHome' className='NavButtons' >Home</button>
+              </a>
+              <a href='PointHistory'>
+                <button className='NavButtons'>Points</button>
+              </a>
+              <a href='CatalogPurchase'>
+                <button className='NavButtons'>Catalog</button>
+              </a>
+              <a href='UpdateAccount'>
+                <button className='NavButtons'>Settings</button>
+              </a>
+            </nav>
+            <ul className='PointHistoryLout-UpAcc'>
+              <img src='DefaultProfPic.png' alt='Default Profile Picure' width='40' height='40'/>
+              <li><a href='UpdateAccount'>Username</a></li>
+              <li><a href='Home'>Logout</a></li>
+            </ul>
+          </header>
           
-        </body>
+          <body>
+            <select className='SortByDrop' id='SortByDrop'>
+              <option disabled selected hidden>Sort By</option>
+              <option>All Time</option>
+              <option>Last 24 Hours</option>
+              <option>Last 7 Days</option>
+              <option>Last 30 Days</option>
+            </select>
+
+            {this.state.points.map((p) => {
+              return(
+                <div className='PointDisplay'>
+                  
+                  <p>Total: {p.totalPoints},  For Sponsor (id): {p.sponsorID}</p>
+                  <p>All adjustments</p>
+                  {p.adjustments.map(a => 
+                    <p>Reason: {a.pointReason}, <span className='Alignment'>Value: {a.pointValue},</span>
+                    <br/> 
+                        Date: {a.date}
+                        <br/><hr/><br/>
+                    </p> 
+                    )}   
+                       
+                </div>
+              )}
+            )}
+          </body>
 
         <footer className='PointHistory-Footer'>
           <img src="teamLogo.png" alt="The Mad Lads Team Logo" width="200" height="70"></img>
@@ -30,6 +63,6 @@ class PointHistory extends Component{
       </div>
     );
   }
-}
+}}
 
 export default PointHistory;
