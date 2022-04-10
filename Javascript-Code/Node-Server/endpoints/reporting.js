84d80efe2a06d30438b4febc2a245533b7859a84
jbinz <jbinz@clemson.edu>
2022-04-10 15:04:26 -0400

    comments

--- Javascript-Code/Node-Server/endpoints/reporting.js
+++ Javascript-Code/Node-Server/endpoints/reporting.js
@@ -1,5 +1,7 @@
 module.exports = function(app, connection){
     
+    // AUDIT LOG REPORTS
+
     app.post('/getLoginAttempts', (req, res) => {
 
         console.log('Recieved Request for Audit Log Report')
@@ -102,11 +104,17 @@ module.exports = function(app, connection){
         console.log(req.body)
 
         // TODO
-        // need to figure out how to deal with the dates
-        // maybe open sqlworkbench & play around with this to see if its the issue
-        // then build out option for no filters.
-        // then add statements for other audit logs
         // then add invoice & sales reporting
+        // build basic sales frontend
+        // report summary sales info for drivers
+        // report detailed sales info for drivers
+        // add filtering inputs? maybe do this before
+        // add by sponsor sales reports
+        // add invoices -- similar to by sponsor
+
+        // add all that but for sponsors
+        // add session compatability to all this
+
 
         const sdate = new Date(req.body.startDate);
         const formattedStartDate = sdate.getFullYear()+'-'+(sdate.getMonth()+1)+'-'+sdate.getDate();
@@ -195,64 +203,5 @@ module.exports = function(app, connection){
 
     });
 
-
-
-
-    // testing new local repo creation
-    
-
-
-    // app.post('/point-assignment', (req, res) => {
-    //     console.log('Recieved point assignment');
-    //     console.log(req.body);
-    //     session=req.session;
-
-    //     const org_query = `SELECT sponsorID from new_schema.USER_SPONSOR_REL where uID = ${session.userid};`;
-    //     connection.query(org_query, function(err, result) {
-    //         if(err) {
-    //             console.log(err);
-    //             res.send({success: false})
-    //         }else{
-    //             sponsorID = result[0].sponsorID;
-    //             const exists_query = `SELECT EXISTS (SELECT * from new_schema.DRIVER_POINTS where uID = ${req.body.driver} AND sponsorID=${sponsorID}) as 'is_exist';`;
-    //             connection.query(exists_query, function(err, result) {
-    //                 if(err) {
-    //                     console.log(err);
-    //                     res.send({success: false})
-    //                 }
-    //                 console.log(result[0].is_exist);
-        
-    //                 if(result[0].is_exist===0){
-    //                     const value = req.body.add ? req.body.value : req.body.value * -1;
-    //                     const points_query = `insert into new_schema.DRIVER_POINTS values (NULL, ${req.body.driver}, ${value}, ${sponsorID});`;
-    //                     connection.query(points_query, function(err, result) {
-    //                         if(err) {
-    //                             console.log(err);
-    //                             console.log("YO")
-    //                             res.send({success: false})
-    //                         }else{
-    //                             addAdjustment(req, sponsorID)
-    //                         }
-    //                     })
-    //                 }else{
-    //                     const driver_point_query = `SELECT * from new_schema.DRIVER_POINTS where uID = ${req.body.driver} AND sponsorID=${sponsorID};`;
-    //                     connection.query(driver_point_query, function(err, result) {
-    //                         const newPoints = req.body.add ? (parseInt(result[0].totalPoints) + parseInt(req.body.value)) : (parseInt(result[0].totalPoints) - parseInt(req.body.value));
-    //                         const points_query = `update new_schema.DRIVER_POINTS set totalPoints = ${newPoints} where uID = ${req.body.driver} AND sponsorID=${sponsorID};`;
-    //                         connection.query(points_query, function(err, result) {
-    //                             if(err) {
-    //                                 console.log(err);
-    //                                 res.send({success: false})
-    //                             }else{
-    //                                 addAdjustment(req, sponsorID)
-    //                             }
-    //                         })
-    //                     })
-    //                 }
-    //             })
-    //         }
-    //     })
-    //     res.send({success: true})
-    // });
 
 }
